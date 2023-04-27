12c8b5bbd88c9c04e709b6cdf9626703d944f41c
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-04-27 19:57:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let browserName=navigator.userAgent.toLowerCase().split("/")[0]
+let browserVerson=navigator.userAget.toLowerCase().split("/")[1]
+let s='You are using '+browserName+" version "+browserVerson;
+function callBack(()=>{
+	alert(s);
+})
