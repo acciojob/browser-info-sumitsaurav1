e5165d7286211e8e4af7195431e6b94178b3568c
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-04-27 22:07:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let browserName=navigator.userAgent.toLowerCase().split("/")[0]
 let browserVerson=navigator.userAget.toLowerCase().split("/")[1]
-let s='You are using '+browserName+" version "+browserVerson;
+let s="You are using "+browserName+" version "+browserVerson;
 
 let div = document.getElementById("browser-info");
 div.innerText=s;
